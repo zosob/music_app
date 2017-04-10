68df294dcbaae52a1898b4f91e001c4b900a1f72
zosob <bhaskar4647@gmail.com>
2017-04-10 14:43:39 +0000

    Updated home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'views#songs'
   resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
